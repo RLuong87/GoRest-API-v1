d44124f3729d3aff57a20fed075d8dd524064dd6
Richies Luong <richiesluong.student@careerdevs.com>
2021-11-18 21:20:47 -0500

    Added Get, Post, Delete, Put methods

--- src/main/java/careerdevs/gateway/gorest/controllers/GoRestUserController.java
+++ src/main/java/careerdevs/gateway/gorest/controllers/GoRestUserController.java
@@ -1,8 +1,15 @@
 package careerdevs.gateway.gorest.controllers;
 
 import careerdevs.gateway.gorest.models.GoRestResponse;
+import careerdevs.gateway.gorest.models.GoRestUsers;
+import careerdevs.gateway.gorest.models.UpdatedUsers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +17,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("/api/gorest/user")
 public class GoRestUserController {
+
+    @Autowired
+    Environment env;
 
     @GetMapping("/pageone")
     public Object allUser(RestTemplate restTemplate) {
@@ -19,19 +29,96 @@ public class GoRestUserController {
     @GetMapping("/get")
     public Object getUser(RestTemplate restTemplate, @RequestParam(name = "id") String id) {
         String URL = "https://gorest.co.in/public/v1/users/" + id;
-        return restTemplate.getForObject(URL, GoRestResponse.class).getData();
+        try {
+
+            return restTemplate.getForObject(URL, GoRestResponse.class).getData();
+
+        } catch (HttpClientErrorException.NotFound e) {
+
+            return "ID did not match a user in the database";
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @PostMapping("/post")
+    public Object postUser(
+            RestTemplate restTemplate,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "email") String email,
+            @RequestParam(name = "gender") String gender,
+            @RequestParam(name = "status") String status
+    ) {
+        String URL = "https://gorest.co.in/public/v1/users/";
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(env.getProperty("gorest.token"));
+
+            GoRestUsers newUser = new GoRestUsers(name, email, gender, status);
+
+            HttpEntity<GoRestUsers> request = new HttpEntity(newUser, headers);
+
+            return restTemplate.exchange(URL, HttpMethod.POST, request, GoRestResponse.class);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+//    @PutMapping("/put/{id}")
+//    public ResponseEntity<GoRestUsers> updateUser(
+//
+//    }
+
+    @PutMapping("/put")
+    public Object updateUsers(
+            RestTemplate restTemplate,
+            @RequestParam(name = "id") String id,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "email") String email,
+            @RequestParam (name = "gender") String gender,
+            @RequestParam (name = "status") String status
+    ) {
+        String URL = "https://gorest.co.in/public/v1/users/" + id;
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(env.getProperty("gorest.token"));
+
+            UpdatedUsers updateUser = new UpdatedUsers();
+            updateUser.setName(name);
+            updateUser.setEmail(email);
+            updateUser.setGender(gender);
+            updateUser.setStatus(status);
+
+            HttpEntity<GoRestUsers> request = new HttpEntity(updateUser, headers);
+
+            return restTemplate.exchange(URL, HttpMethod.PUT, request, GoRestResponse.class);
+
+        } catch (Exception e) {
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
     }
 
     @DeleteMapping("/delete")
     public Object deleteUser(RestTemplate restTemplate, @RequestParam(name = "id") String id) {
+
         String URL = "https://gorest.co.in/public/v1/users/" + id;
         try {
 
             HttpHeaders headers = new HttpHeaders();
+            headers.setBearerAuth(env.getProperty("gorest.token"));
 
             HttpEntity request = new HttpEntity(headers);
 
-            restTemplate.delete(URL, request);
+            restTemplate.exchange(URL, HttpMethod.DELETE, request, GoRestResponse.class);
+
             return "Successfully deleted user: " + id;
 
         } catch (HttpClientErrorException.Unauthorized e) {
---
+++ src/main/java/careerdevs/gateway/gorest/models/GoRestUsers.java
@@ -0,0 +1,50 @@
+package careerdevs.gateway.gorest.models;
+
+public class GoRestUsers {
+    private String name;
+    private String email;
+    private String gender;
+    private String status;
+
+    public GoRestUsers(String name,
+                       String email,
+                       String gender,
+                       String status) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
